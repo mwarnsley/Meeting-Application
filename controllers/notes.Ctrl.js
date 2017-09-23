9bde850a262505df69afdf98eb4adce34be66a2a
Marcus Warnsley <marcus@PC0159-Jscott.local>
2017-09-23 17:42:56 -0400

    Finishing the app adding in the possibility to filter the notes on the homepage

--- controllers/notes.Ctrl.js
+++ controllers/notes.Ctrl.js
@@ -3,6 +3,7 @@
 const Note = require('../models/Note.model');
 const User = require('../models/User.model');
 
+// Export the function to get all of the users notes
 exports.allUsersNotes = (req, res) => {
   // Find all of the users
   User.find({})
@@ -21,6 +22,7 @@ exports.allUsersNotes = (req, res) => {
     });
 };
 
+// Exporting the function to create a new note
 exports.createNote = (req, res) => {
   // Creating a new note
   const newNote = new Note();
@@ -43,4 +45,27 @@ exports.createNote = (req, res) => {
       res.redirect(301, '/');
     }
   });
+};
+
+// Exporting the function to filter by name
+exports.noteByMember = (req, res) => {
+  const query = Note.find({});
+  const filter = req.body.memberName;
+
+  if (filter.length === 0) {
+    console.log('No Notes Found');
+  } else {
+    query
+      .where({
+        memberName: filter,
+      })
+      .sort({
+        createdOn: 'desc',
+      })
+      .exec((err, results) => {
+        res.render('index', {
+          notes: results,
+        });
+      });
+  }
 };
