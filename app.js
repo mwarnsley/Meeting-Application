e38b1c4bd84af6c30a032cbade557467a9baac78
Marcus Warnsley <marcus@PC0159-Jscott.local>
2017-09-23 16:41:48 -0400

    Adding in the index.html file and the css styles using swig

---
+++ app.js
@@ -0,0 +1,37 @@
+// Requiring in all of the modules to use
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const swig = require('swig');
+const routes = require('./routes/index');
+const db = require('./config/keys').db;
+
+// Starting the application up and setting up the default port if one isn't available
+const app = express();
+const port = 5000;
+
+// Setting up the port to equal the environment port OR the default variable port
+const PORT = process.env.PORT || port;
+
+// Connecting the mongoose database to the database
+mongoose.connect(db);
+
+// Setup View Engine
+app.engine('html', swig.renderFile);
+
+// Since we are using a view engine we are using app set for views
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html');
+
+// Using the middleware for express and our routes
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', routes);
+
+app.get('/', (req, res) => {
+  res.render('index.html');
+});
+
+app.listen(PORT, () => {
+  console.log(`App is listening on port: ${PORT}`);
+});
